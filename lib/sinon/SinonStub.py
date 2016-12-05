83b30e9e911ae71512c48a4e54c924d79fb7c9d7
Kir Chou <note351@hotmail.com>
2016-12-05 17:37:25 +0800

    adjust inherit relation

--- lib/sinon/SinonStub.py
+++ lib/sinon/SinonStub.py
@@ -2,29 +2,32 @@ import sys
 sys.path.insert(0, '../')
 
 from lib.sinon.util import ErrorHandler, Wrapper, CollectionHandler
-from lib.sinon.SinonBase import SinonBase
+from lib.sinon.SinonSpy import SinonSpy
 
-class SinonStub(SinonBase):
+class SinonStub(SinonSpy):
 
     def __init__(self, obj=None, prop=None, func=None):
         super(SinonStub, self).__init__(obj, prop)
+        self._prepare(func)
+
+    def _prepare(self, func):
         self.stubfunc = func if func else Wrapper.emptyFunction
         super(SinonStub, self).addWrapStub(self.stubfunc)
         self.condition = {"args":[], "kwargs":[], "action": [], "oncall":[]}
-        self.args = self.kwargs = self.oncall = None
+        self.cond_args = self.cond_kwargs = self.oncall = None
 
     def _appendCondition(self, func):
-        self.condition["args"].append(self.args)
-        self.condition["kwargs"].append(self.kwargs)
+        self.condition["args"].append(self.cond_args)
+        self.condition["kwargs"].append(self.cond_kwargs)
         self.condition["oncall"].append(self.oncall)
         self.condition["action"].append(func)
-        self.args = self.kwargs = self.oncall = None
+        self.cond_args = self.cond_kwargs = self.oncall = None
 
     def withArgs(self, *args, **kwargs):
         if args:
-            self.args = args
+            self.cond_args = args
         if kwargs:
-            self.kwargs = kwargs
+            self.cond_kwargs = kwargs
         return self
 
     def onCall(self, n):
@@ -46,7 +49,7 @@ class SinonStub(SinonBase):
     def returns(self, obj):
         def returnFunction(*args, **kwargs):
             return obj
-        if self.args or self.kwargs or self.oncall:
+        if self.cond_args or self.cond_kwargs or self.oncall:
             self._appendCondition(returnFunction)
             super(SinonStub, self).addWrapStub(self.stubfunc, self.condition)
         else:
@@ -56,7 +59,7 @@ class SinonStub(SinonBase):
     def throws(self, exceptions=Exception):
         def exceptionFunction(*args, **kwargs):
             raise exceptions
-        if self.args or self.kwargs or self.oncall:
+        if self.cond_args or self.cond_kwargs or self.oncall:
             self._appendCondition(exceptionFunction)
             super(SinonStub, self).addWrapStub(self.stubfunc, self.condition)
         else:
