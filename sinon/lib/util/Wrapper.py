fe7470f50e835a045e99815c3934302bc24a393d
Kir Chou <note351@hotmail.com>
2016-12-21 03:08:33 +0800

    clean code: stub in the wrapSpy function

--- sinon/lib/util/Wrapper.py
+++ sinon/lib/util/Wrapper.py
@@ -32,39 +32,62 @@ def addSpy(f):
     wrapped.ret_list = []
     wrapped.LOCK = True
     return wrapped
+   
+
+def _genIndexList(condition, *args, **kwargs): 
+    # Generating index list based on arguments and condition
+    # Note: ignore args[0] because it is callback in this condition
+    # combination
+    if args[1:] and kwargs:
+        if args[1:] in condition["args"] and kwargs in condition["kwargs"]:
+            args_indices = [i for i, x in enumerate(condition["args"]) if x == args[1:]]
+            kwargs_indices = [i for i, x in enumerate(condition["kwargs"]) if x == kwargs]
+            return (list(set(args_indices).intersection(kwargs_indices)))
+    # args only
+    elif args[1:]:
+        if args[1:] in condition["args"]:
+            return [i for i, x in enumerate(condition["args"]) if x == args[1:] and not condition["kwargs"][i]]
+    #kwargs only
+    elif kwargs:
+        if kwargs in condition["kwargs"]:
+            return [i for i, x in enumerate(condition["kwargs"]) if x == kwargs and not condition["args"][i]]
+
+
+def _genRetFuncWithArgs(index_list, condition, fn, *args, **kwargs):
+    """
+    @return customfunc by condition(args/kwargs || args+oncall/kwargs+oncall)
+    """
+    for i in reversed(index_list):
+        if not condition["oncall"][i] or condition["oncall"][i] == fn.callCount:
+            return condition["action"][i](*args, **kwargs)
+
+
+def _genRetFuncWithoutArgs(index_list, condition, fn, *args, **kwargs):
+    """
+    @return customfunc by condition(oncall)
+    """
+    if fn.callCount in condition["oncall"]:
+        index_list = [i for i, x in enumerate(condition["oncall"]) if x and not condition["args"][i] and not condition["kwargs"][i]]
+        for i in reversed(index_list):
+            if fn.callCount == condition["oncall"][i]:
+                return condition["action"][i](*args, **kwargs)
+
+
+def _genStubFunc(customfunc, condition, fn, *args, **kwargs):
+    if condition:
+        index_list = _genIndexList(condition, *args, **kwargs)
+        if index_list:
+            return _genRetFuncWithArgs(index_list, condition, fn, *args, **kwargs)
+        return _genRetFuncWithoutArgs(index_list, condition, fn, *args, **kwargs)
+    #return stub function
+    return customfunc(*args, **kwargs)
 
 
 def wrapSpy(f, customfunc=None, condition=None):
     if customfunc:
         @addSpy
         def fn(*args, **kwargs):
-            if condition:
-                if args[1:] and kwargs:
-                    if args[1:] in condition["args"] and kwargs in condition["kwargs"]:
-                        args_indices = [i for i, x in enumerate(condition["args"]) if x == args[1:]]
-                        kwargs_indices = [i for i, x in enumerate(condition["kwargs"]) if x == kwargs]
-                        index_list = (list(set(args_indices).intersection(kwargs_indices)))
-                        for i in reversed(index_list):
-                            if not condition["oncall"][i] or condition["oncall"][i] == fn.callCount:
-                                return condition["action"][i](*args, **kwargs)
-                elif args[1:]:
-                    if args[1:] in condition["args"]:
-                        index_list = [i for i, x in enumerate(condition["args"]) if x == args[1:] and not condition["kwargs"][i]]
-                        for i in reversed(index_list):
-                            if not condition["oncall"][i] or condition["oncall"][i] == fn.callCount:
-                                return condition["action"][i](*args, **kwargs)
-                elif kwargs:
-                    if kwargs in condition["kwargs"]:
-                        index_list = [i for i, x in enumerate(condition["kwargs"]) if x == kwargs and not condition["args"][i]]
-                        for i in reversed(index_list):
-                            if not condition["oncall"][i] or condition["oncall"][i] == fn.callCount:
-                                return condition["action"][i](*args, **kwargs)
-                if fn.callCount in condition["oncall"]:
-                    index_list = [i for i, x in enumerate(condition["oncall"]) if x and not condition["args"][i] and not condition["kwargs"][i]]
-                    for i in reversed(index_list):
-                        if fn.callCount == condition["oncall"][i]:
-                            return condition["action"][i](*args, **kwargs)
-            return customfunc(*args, **kwargs)
+            return _genStubFunc(customfunc, condition, fn, *args, **kwargs)
     else:
         @addSpy
         def fn(*args, **kwargs):
