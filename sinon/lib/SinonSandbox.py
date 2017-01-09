eb7c83447e570e552d43be72c130e5d710a65705
Kir Chou <note351@hotmail.com>
2017-01-09 23:30:21 +0800

    pylint: 10/10 (SinonSandbox)

--- sinon/lib/SinonSandbox.py
+++ sinon/lib/SinonSandbox.py
@@ -1,72 +1,60 @@
+'''
+Copyright (c) 2016-2017, Kir Chou
+https://github.com/note35/sinon/blob/master/LICENSE
+'''
 from .SinonBase import SinonBase
 
-properties = ["SinonSpy", "SinonStub", "SinonMock", "SinonAssertion"]
-production_properties = ["spy", "stub", "mock", "assert"]
-
-def _clear_assertion_message(obj):
+def __clear_assertion_message(obj):
+    """
+    Clearing assertion message
+    """
     setattr(obj, "message", "")
 
-def _clear_item_in_queue(queue):
+def __clear_item_in_queue(queue):
+    """
+    Clearing all items in the queue
+    Ignoring destroied item
+    """
     for item in reversed(queue):
         try:
             item.restore()
-        except:
-            """
-            ignore if the item is been destroyed before here
-            """
+        except: #pylint: disable=bare-except
             pass
 
-def sinontest(f):
+def sinontest(test_func):
+    """
+    Wrapping test functions
+    """
+    properties = ["SinonSpy", "SinonStub", "SinonMock", "SinonAssertion"]
+    production_properties = ["spy", "stub", "mock", "assert"]
 
-
-    def fn(*args, **kwargs):
+    def wrapped_func(*args, **kwargs): #pylint: disable=missing-docstring
 
         # store original _queue
         original_queue = []
-        for item in SinonBase._queue:
+        for item in SinonBase._queue: #pylint: disable=protected-access
             original_queue.append(item)
 
-        ret = f(*args, **kwargs)
+        ret = test_func(*args, **kwargs)
 
-        # handle production mode (called by sinon.py)
+        # handle indirect use (called by sinon.py)
         for prop in production_properties:
-            if "sinon" in f.__globals__ and prop in dir(f.__globals__["sinon"]):
+            if "sinon" in test_func.__globals__ and prop in dir(test_func.__globals__["sinon"]):
                 if prop == "assert":
-                    _clear_assertion_message(getattr(f.__globals__["sinon"], prop))
+                    __clear_assertion_message(getattr(test_func.__globals__["sinon"], prop))
                 else:
-                    _clear_item_in_queue(getattr(f.__globals__["sinon"], prop)._queue)
+                    __clear_item_in_queue(getattr(test_func.__globals__["sinon"], prop)._queue) #pylint: disable=protected-access
 
-        # handle unittest (direct use)
+        # handle direct use
         for prop in properties:
-            if prop in f.__globals__.keys():
+            if prop in test_func.__globals__.keys():
                 if prop == "SinonAssertion":
-                    _clear_assertion_message(f.__globals__[prop])
+                    __clear_assertion_message(test_func.__globals__[prop])
                 else:
-                    _clear_item_in_queue(f.__globals__[prop]._queue)
+                    __clear_item_in_queue(test_func.__globals__[prop]._queue) #pylint: disable=protected-access
 
         # set original _queue value back
-        SinonBase._queue = original_queue
+        SinonBase._queue = original_queue #pylint: disable=protected-access
 
         return ret
-    return fn
-
-
-class SinonSandbox(object):
-
-    def __init__(self):
-        pass
-
-    def create(self, config=None):
-        pass
-
-    def spy(self):
-        pass
-
-    def stub(self):
-        pass
-
-    def mock(self):
-        pass
-
-    def restore(self):
-        pass
+    return wrapped_func
