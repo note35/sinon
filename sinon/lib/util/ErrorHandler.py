997f46508679ae6dd657f976f4b7cd1aa0201468
Kir Chou <note351@hotmail.com>
2017-01-17 20:37:13 +0800

    pylint: 10/10 (util/ErrorHandler)

--- sinon/lib/util/ErrorHandler.py
+++ sinon/lib/util/ErrorHandler.py
@@ -5,50 +5,51 @@ https://github.com/note35/sinon/blob/master/LICENSE
 A set of functions for handling known error
 """
 
-def __exception_helper(msg, exception=Exception):
+def __exception_helper(msg, exception=Exception): #pylint: disable=missing-docstring
     raise exception(msg)
 
-def mock_type_error(obj):
+def mock_type_error(obj): #pylint: disable=missing-docstring
     error_msg = "[{}] is an invalid module/class".format(str(obj))
     return __exception_helper(error_msg)
 
-def prop_type_error(prop):
+def prop_type_error(prop): #pylint: disable=missing-docstring
     error_msg = "[{}] is an invalid property, it should be a string".format(prop)
     return __exception_helper(error_msg)
 
-def prop_is_func_error(obj, prop):
-    error_msg = "[{}] is an invalid property, it should be a method in [{}]".format(prop, obj.__name__)
+def prop_is_func_error(obj, prop): #pylint: disable=missing-docstring
+    name = obj.__name__ if hasattr(obj, "__name__") else obj
+    error_msg = "[{}] is an invalid property, it should be a method in [{}]".format(prop, name)
     return __exception_helper(error_msg)
 
-def prop_in_obj_error(obj, prop):
+def prop_in_obj_error(obj, prop): #pylint: disable=missing-docstring
     error_msg = "[{}] is not exist in [{}]".format(prop, obj)
     return __exception_helper(error_msg)
 
-def lock_error(obj):
+def lock_error(obj): #pylint: disable=missing-docstring
     name = obj.__name__ if hasattr(obj, "__name__") else obj
     error_msg = "[{}] have already been declared".format(name)
     return __exception_helper(error_msg)
 
-def called_with_empty_error():
+def called_with_empty_error(): #pylint: disable=missing-docstring
     error_msg = "There is no argument"
     return __exception_helper(error_msg)
 
-def get_callqueue_index_error(number):
+def get_callqueue_index_error(number): #pylint: disable=missing-docstring
     error_msg = "The call queue only contains {} calls".format(str(number))
     return __exception_helper(error_msg, exception=IndexError)
 
-def is_not_spy_error(obj):
+def is_not_spy_error(obj): #pylint: disable=missing-docstring
     error_msg = "[{}] is an invalid spy".format(str(obj))
     return __exception_helper(error_msg)
 
-def callqueue_is_empty_error():
+def callqueue_is_empty_error(): #pylint: disable=missing-docstring
     error_msg = "CALLQUEUE is empty"
     return __exception_helper(error_msg)
 
-def matcher_type_error(prop):
+def matcher_type_error(prop): #pylint: disable=missing-docstring
     error_msg = "[{}] is an invalid property, it should be a type".format(prop)
     return __exception_helper(error_msg, exception=TypeError)
 
-def matcher_instance_error(prop):
+def matcher_instance_error(prop): #pylint: disable=missing-docstring
     error_msg = "[{}] is an invalid property, it should be an instance".format(prop)
     return __exception_helper(error_msg, exception=TypeError)
