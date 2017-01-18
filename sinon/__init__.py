eb6dde66d6ffc0047dcc5d3354b52a1e0cc0c1cc
Kir Chou <note351@hotmail.com>
2017-01-18 23:09:30 +0800

    add missing lib

--- sinon/__init__.py
+++ sinon/__init__.py
@@ -1,7 +1,9 @@
-from .lib import SinonBase, SinonSpy, SinonStub, SinonMock, SinonSandbox
+from .lib import SinonBase, SinonSpy, SinonStub, SinonMock, SinonSandbox, SinonAssertion, SinonMatcher 
 
 init = SinonBase.init
 test = SinonSandbox.sinontest
 stub = SinonStub.SinonStub
 spy = SinonSpy.SinonSpy
 mock = SinonMock.SinonMock
+assertion = SinonAssertion.SinonAssertion
+match = SinonMatcher.SinonMatcher
