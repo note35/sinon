009e91aa659ba3a1a8fd4341afc238900644cb5e
Kir Chou <note351@hotmail.com>
2017-02-24 15:24:41 +0800

    prepend copyright

--- sinon/__init__.py
+++ sinon/__init__.py
@@ -1,3 +1,7 @@
+"""
+Copyright (c) 2016-2017, Kir Chou
+https://github.com/note35/sinon/blob/master/LICENSE
+"""
 from .lib import base, spy, stub, mock, sandbox, assertion, matcher 
 
 init = base.init
